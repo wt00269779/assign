ce76732329c45cad5e9a3868c309f9bfe6260b07
Saksit <saksitpz1011@gmail.com>
2016-10-23 00:35:25 +0700

    Sucess

--- assign9-1.c
+++ assign9-1.c
@@ -2,12 +2,12 @@
 #include<conio.h>
 #include<time.h>
 
-void load_data_file_text(char filename[],double *data,int *count){
+void load_data_file_text(char filename[],int *data,int *count){
 	FILE *fp;
-	double a;
+	int a;
 	if((fp=fopen(filename,"r"))!=NULL){
 		*count = 0;
-		while(fscanf(fp,"%lf",&a)==1){
+		while(fscanf(fp,"%d",&a)==1){
 			data[*count]=a;
 			*count = *count + 1;
 		}
@@ -34,22 +34,7 @@ void load_data_file_binary(char filename[],int *data,int *count){
 	}
 }
 
-void order_data_double(double *data,int count){
-	int i,j;
-	double buff;
-	for(i=0;i<count-1;i++){
-		for(j=i+1;j<count;j++){
-			if(data[i]>data[j]){
-				buff = data[i];
-				data[i] = data[j];
-				data[j] = buff;
-			}
-		}
-	}
-	printf("\nOrder data double success!\n");
-}
-
-void quick_sort_d(double arr[],int low,int high){
+void quick_sort(int arr[],int low,int high){
  int pivot,j,temp,i;
  if(low<high){
   pivot = low;
@@ -71,52 +56,20 @@ void quick_sort_d(double arr[],int low,int high){
   temp=arr[pivot];
   arr[pivot]=arr[j];
   arr[j]=temp;
-  quick_sort_d(arr,low,j-1);
-  quick_sort_d(arr,j+1,high);
+  quick_sort(arr,low,j-1);
+  quick_sort(arr,j+1,high);
  }
 }
 
-
-void quick_sort_i(int arr[],int low,int high){
- int pivot,j,temp,i;
- if(low<high){
-  pivot = low;
-  i = low;
-  j = high;
-  while(i<j){
-   while((arr[i]<=arr[pivot])&&(i<high)){
-    i++;
-   }
-   while(arr[j]>arr[pivot]){
-    j--;
-   }
-   if(i<j){ 
-    temp=arr[i];
-    arr[i]=arr[j];
-    arr[j]=temp;
-   }
-  }
-  temp=arr[pivot];
-  arr[pivot]=arr[j];
-  arr[j]=temp;
-  quick_sort_i(arr,low,j-1);
-  quick_sort_i(arr,j+1,high);
- }
-}
-
-int quick_sr(double *data1,int *data2,int count1,int count2,int *same_num){
+int find_same_data(int *data1,int *data2,int count1,int count2,int *same_data){
 	int i=0,j=0;
 	int same_count=0;
-	*same_num=0;
 	while(i<count1&&j<count2){
 		if(data1[i]>=data2[j]){
 			if(data1[i]==data2[j]){
-				same_num[same_count++] = data2[j];
-				i++;
+				same_data[same_count++] = data2[j];
 			}
-			else{ 
-				j++;
-			}
+			j++;
 		}
 		else{
 			i++;
@@ -126,10 +79,8 @@ int quick_sr(double *data1,int *data2,int count1,int count2,int *same_num){
 }
 
 int main(){
-	double data1[120000];
+	int data1[120000],data2[120000],same_data[2000];
 	double start,stop,total;
-	int data2[120000];
-	int same_data[2000];
 	int count_d1,count_d2,same_count,i;
 	char file1[50] = "txt100000.txt",file2[50] = "bin100000.bin";
 	load_data_file_text(file1,data1,&count_d1);
@@ -137,11 +88,11 @@ int main(){
 	printf("\nRead text file from %s total %d records\n",file1,count_d1);
 	printf("Read binary file frim %s total %d records\n",file2,count_d2);
 	printf("Total data = %d records\n",count_d1+count_d2);
-	quick_sort_d(data1,0,count_d1);
-	quick_sort_i(data2,0,count_d2);
-	printf("\nProcess data\n");
 	start = clock();
-	same_count = quick_sr(data1,data2,count_d1,count_d2,same_data);
+	quick_sort(data1,0,count_d1);
+	quick_sort(data2,0,count_d2);
+	printf("\nProcess data\n");
+	same_count = find_same_data(data1,data2,count_d1,count_d2,same_data);
 	stop = clock();
 	for(i=1;i<same_count;i++){
 		printf("Found [%d] = %d\n",i,same_data[i]);
